9ec4a4301d57e904a2f5cd739542d7c3d8af9e58
Damon <126731021+damon314159@users.noreply.github.com>
2024-04-07 02:34:05 +0100

    feat: score counter for clicking unique items

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-// eslint-disable-next-line import/no-absolute-path
-import viteLogo from '/vite.svg'
+import {
+  type Dispatch,
+  type SetStateAction,
+  type MouseEvent,
+  type KeyboardEvent,
+  useState
+} from 'react'
+import { v4 as uuid } from 'uuid'
 import './App.css'
+import type ClickableItem from './types/ClickableItem'
+import ItemPanel from './components/itemPanel/ItemPanel'
+import Scoreboard from './components/scoreboard/Scoreboard'
 
-function App() {
-  const [count, setCount] = useState(0)
+function clickHandlerFactoryFactory(
+  items: ClickableItem[],
+  setItems: Dispatch<SetStateAction<ClickableItem[]>>,
+  score: number,
+  setScore: Dispatch<SetStateAction<number>>,
+  highScore: number,
+  setHighScore: Dispatch<SetStateAction<number>>
+): (ID: string) => (e: MouseEvent | KeyboardEvent) => void {
+  // Provide state to returned clickHandlerFactory via closure.
+  // This can be passed down with props
+  return (ID: string): ((e: MouseEvent | KeyboardEvent) => void) =>
+    (e: MouseEvent | KeyboardEvent): void => {
+      if ('key' in e && e.key !== 'Enter') {
+        return
+      }
+      // TODO Guard clause for no focus on KB event
+      if (
+        items.find((item: ClickableItem): boolean => item.ID === ID)?.wasClicked
+      ) {
+        setItems(
+          items.map(
+            (item: ClickableItem): ClickableItem => ({
+              ...item,
+              wasClicked: false
+            })
+          )
+        )
+        // TODO Shuffle array
+        setHighScore(Math.max(score, highScore))
+        setScore(0)
+        return
+      }
+      setItems(
+        items.map(
+          (item: ClickableItem): ClickableItem =>
+            item.ID === ID ? { ...item, wasClicked: true } : item
+        )
+      )
+      // TODO Shuffle array
+      setScore(score + 1)
+    }
+}
+
+function App(): JSX.Element {
+  const [items, setItems] = useState(
+    Array.from(
+      { length: 10 },
+      (): ClickableItem => ({
+        ID: uuid(),
+        imageURL: '',
+        wasClicked: false
+      })
+    )
+  )
+  // TODO Use Effect to fetch and insert URLS to state
+  const [score, setScore] = useState(0)
+  const [highScore, setHighScore] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          type="button"
-          onClick={() => {
-            setCount((c) => c + 1)
-          }}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <h1>Memory Game</h1>
+      <Scoreboard score={score} highScore={highScore} />
+      <ItemPanel
+        items={items}
+        clickHandlerFactory={clickHandlerFactoryFactory(
+          items,
+          setItems,
+          score,
+          setScore,
+          highScore,
+          setHighScore
+        )}
+      />
     </>
   )
 }
