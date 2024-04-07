2e78149f1aefc207102374de7e5050cc9f7ef6a8
Damon <126731021+damon314159@users.noreply.github.com>
2024-04-07 01:06:45 +0100

    feat: bare bones score counter

---
+++ src/components/scoreboard/Scoreboard.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  score: number
+  highScore: number
+}
+
+function Scoreboard({ score, highScore }: Props): JSX.Element {
+  return (
+    <>
+      <div>Current score: {score}</div>
+      <div>High score: {highScore}</div>
+    </>
+  )
+}
+
+export default Scoreboard
